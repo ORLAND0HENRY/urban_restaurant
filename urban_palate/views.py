bd48d622181f992355c49fe1e071161612cf8427
ORLAND0HENRY <icespice827@gmail.com>
2026-08-18 19:55:42 +0300

    final final comit

--- urban_palate/views.py
+++ urban_palate/views.py
@@ -1,26 +1,17 @@
-from django.shortcuts import redirect
-from django.contrib.auth import views as auth_views
-
 from django.shortcuts import render, redirect
+from menu.models import Category
+
 
 def home_view(request):
-    # 1. Check if the user is authenticated (logged in)
-    if request.user.is_authenticated:
+    # Fetch categories along with their available items
+    categories = Category.objects.prefetch_related('items').all()
 
-        # 2. Check if the user is a staff/admin user
+    if request.user.is_authenticated:
         if request.user.is_staff:
-            # If they are staff, redirect them to the namespaced dashboard home
             return redirect('dashboard:home')
 
-        # 3. If they are authenticated but NOT staff (i.e., a normal customer)
-        else:
-            # Redirect them to the main authenticated normal users view (index.html)
+        # Pass categories context for authenticated regular users
+        return render(request, 'landing.html', {'categories': categories})
 
-            return render (request,'index.html')
-
-    # 4. If the user is NOT authenticated (anonymous visitor)
-    else:
-        # Redirect them to the custom landing page (landing.html)
-
-        return render(request,'landing.html')
-
+    # Pass categories context for anonymous landing visitors as well
+    return render(request, 'landing.html', {'categories': categories})
